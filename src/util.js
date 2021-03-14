09f9ab2a05d94ee8ba692ab05dcfd262f5dc99f5
istiaqueahmedarik <69030256+istiaqueahmedarik@users.noreply.github.com>
2021-03-14 23:40:59 +0600

    Update util.js

--- src/util.js
+++ src/util.js
@@ -1,57 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 function chillHop() {
   return [
-    
-    {
-      name: "Beaver Creek",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
-      artist: "Aso, Middle School, Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
-      color: ["#205950", "#2ab3bf"],
-      id: uuidv4(),
-      active: true,
-    },
-    {
-      name: "Daylight",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
-      artist: "Aiguille",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
-      color: ["#EF8EA9", "#ab417f"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Keep Going",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
-      artist: "Swørn",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
-      color: ["#CD607D", "#c94043"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Nightfall",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
-      artist: "Aiguille",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
-      color: ["#EF8EA9", "#ab417f"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Reflection",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
-      artist: "Swørn",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
-      color: ["#CD607D", "#c94043"],
-      id: uuidv4(),
-      active: false,
-    },
     {
       name: "Under the City Stars",
       cover:
@@ -62,6 +11,57 @@ function chillHop() {
       id: uuidv4(),
       active: false,
     },
+    {
+      name: "বাংলাটা ঠিক আসে না",
+      cover:
+        "https://scontent.fdac99-1.fna.fbcdn.net/v/t1.0-9/160017260_1386304888409895_7415905677899883193_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=b9115d&_nc_eui2=AeGX4ZonhkhdU0dqehFmMa9R_xPtFHCnjhz_E-0UcKeOHDZbDmvTMu3ZMZQ7uB7LhOZ5P-WbISYxXbpqI3p344oq&_nc_ohc=A7rontR32nEAX_xb1d5&_nc_ht=scontent.fdac99-1.fna&oh=81410923044128f8eef478e1e8b11d0d&oe=607469C1",
+      artist: "ভবানীপ্রসাদ মজুমদার",
+      audio: "https://mcdn.podbean.com/mf/web/czy4xn/8f85x.mp3",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "মাগো ওরা বলে",
+      cover:
+        "https://scontent.fdac99-1.fna.fbcdn.net/v/t1.0-0/p180x540/160558005_1386305061743211_7006432533857480778_o.jpg?_nc_cat=105&ccb=1-3&_nc_sid=b9115d&_nc_eui2=AeHfQwLvDqJG2ZueWtxuvukYd5tDWi-ZwHp3m0NaL5nAekNr8IYBvtoy5o2xeLjFu8KWhuZjcQ5vG8pkfinkZ97o&_nc_ohc=eWb02LiXOacAX_zM3vC&_nc_ht=scontent.fdac99-1.fna&tp=6&oh=d6bed92c820bea8a7d45e59708d4bac0&oe=60738985",
+      artist: "কবি আবু জাফর ওবায়দুল্লাহ",
+      audio: "https://mcdn.podbean.com/mf/web/3zq8rj/magoOra.mp3",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Kidnapped",
+      cover:
+        "https://scontent.fdac99-1.fna.fbcdn.net/v/t1.0-9/160201943_1386304918409892_5660776033323138756_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=b9115d&_nc_eui2=AeFCA_7X_Z7OsSD9l_c-4T4Tb3hZvcaYey5veFm9xph7LuZeMss9f7vlsHL1EahDoUQtNz6dYwr6eT0bMf97-8kG&_nc_ohc=e3kcG7BcOIsAX-Wt76d&_nc_ht=scontent.fdac99-1.fna&oh=e77dfac69f349e360aea167e08d5d464&oe=60752934",
+      artist: "Md Sadat Alam Protik",
+      audio: "https://mcdn.podbean.com/mf/web/7u8u8f/KidnappedAudioNatokDrama.mp3",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "ইর্ষা", 
+      cover:
+        "https://scontent.fdac99-1.fna.fbcdn.net/v/t1.0-9/160924010_1386304998409884_327000006837339307_o.jpg?_nc_cat=100&ccb=1-3&_nc_sid=b9115d&_nc_eui2=AeEcK3IwWGXoIfyEUNJWM7jOr_u9sl5qA-uv-72yXmoD60TiaB7ulRiULG2Db6FADnEx7OmsqpBZ3Cl7cO18uNYg&_nc_ohc=5qSY378fFe4AX9KSN-o&_nc_ht=scontent.fdac99-1.fna&oh=312430ee6ea8aec88b52bcfba9627b1a&oe=6073D81D",
+      artist: "Md Sadat Alam Protik",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "তোমায় আমি চিনবোনা",
+      cover:
+        "https://scontent.fdac99-1.fna.fbcdn.net/v/t1.0-9/160612449_1386305038409880_8657970991707352928_o.jpg?_nc_cat=105&ccb=1-3&_nc_sid=b9115d&_nc_eui2=AeFlXJWHqZjz6GKqZm_aDQrjS62waIQdapBLrbBohB1qkCYUjRGzfogqSosR2rhmAoEBX3oPNUbjz0juTfPyAGX6&_nc_ohc=tfUg_UZmkp0AX8zjlR-&_nc_ht=scontent.fdac99-1.fna&oh=992f8436b995abe4f525e2252464ee00&oe=60745F16",
+      artist: "Md Sadat Alam Protik",
+      audio: "https://mcdn.podbean.com/mf/web/cixkvy/863w2.mp3",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    
     //ADD MORE HERE
     
   ];
